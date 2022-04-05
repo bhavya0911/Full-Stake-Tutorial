55f3e9c414b6f288dfafef90ceb6f74a890fab67
bhavya0911 <bhavya0911@gmail.com>
2022-04-05 12:27:56 +0000

    Try 4 HRM_HOST

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
 		adapter: adapter(),
 		vite: {
 			server: {
-				hrm: {
+				hmr: {
 					clientPort: process.env.HMR_HOST ? 443:3000,
 					host: process.env.HMR_HOST ? process.env.HMR_HOST.substring("https://").length : "localhost"
 				}
